574f875d760c47f6b1f405eebdd97616d81900b1
nonameofthisaccount11 <valack911@gmail.com>
2025-03-24 14:50:38 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ module.exports = {
     browser: [process.env.BOT_NAME, process.env.BROWSER_NAME, process.env.BROWSER_VERSION],
     maxReconnects: parseInt(process.env.MAX_RECONNECTS) || 5,
     hackerMode: process.env.HACKER_MODE === "true",
-    ownerNumber: process.env.OWNER_NUMBER || "1234567890@s.whatsapp.net"  // Replace with your number
+    ownerNumber: process.env.OWNER_NUMBER || "923251584143@s.whatsapp.net"  // Replace with your number
 };
