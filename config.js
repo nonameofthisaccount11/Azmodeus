51e33f0fc0c6a09fb0511322286b60f0b44bf735
nonameofthisaccount11 <valack911@gmail.com>
2025-03-24 14:40:11 +0500

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,7 @@ require('dotenv').config();
 module.exports = {
     botName: process.env.BOT_NAME || "Azmodeus-Bot",
     browser: [process.env.BOT_NAME, process.env.BROWSER_NAME, process.env.BROWSER_VERSION],
-    maxReconnects: parseInt(process.env.MAX_RECONNECTS) || 5
+    maxReconnects: parseInt(process.env.MAX_RECONNECTS) || 5,
+    hackerMode: process.env.HACKER_MODE === "true",
+    ownerNumber: process.env.OWNER_NUMBER || "1234567890@s.whatsapp.net"  // Replace with your number
 };
